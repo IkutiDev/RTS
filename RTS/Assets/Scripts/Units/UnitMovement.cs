f71f6adfa44664b64d36e824db9b7e5b1333e7bf
Ikuti <m.j.olkiewicz@gmail.com>
2021-03-26 00:55:51 +0100

    Added unit spawners

--- RTS/Assets/Scripts/Units/UnitMovement.cs
+++ RTS/Assets/Scripts/Units/UnitMovement.cs
@@ -5,50 +5,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
-
-[RequireComponent(typeof(NavMeshAgent))]
-public class UnitMovement : NetworkBehaviour
+namespace RTS.Units
 {
-    [SerializeField] private NavMeshAgent navMeshAgent = null;
+    [RequireComponent(typeof(NavMeshAgent))]
 
-    private Camera mainCamera;
-    private NavMeshHit hit; 
-    #region Server
-    [Command]
-    private void CmdMove(Vector3 position)
+    public class UnitMovement : NetworkBehaviour
     {
-        if(!ValidatePosition(position)) { return; }
-        navMeshAgent.SetDestination(hit.position);
-    }
+        [SerializeField] private NavMeshAgent navMeshAgent = null;
 
-    private bool ValidatePosition(Vector3 position)
-    {
-        if (!NavMesh.SamplePosition(position, out NavMeshHit navMeshHit, 1f, NavMesh.AllAreas)) return false;
-        hit = navMeshHit;
-        return true;
-    }
-    #endregion
-    #region Client
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        navMeshAgent = GetComponent<NavMeshAgent>();
-    }
-    public override void OnStartAuthority()
-    {
-        base.OnStartAuthority();
-        mainCamera = Camera.main;
-    }
-    [ClientCallback]
-    private void Update()
-    {
-        //Checking if right click is valid for moving
-        if (!hasAuthority) return;
-        if (!Mouse.current.rightButton.wasPressedThisFrame) return;
-        Ray ray =mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (!Physics.Raycast(ray, out RaycastHit raycastHit, Mathf.Infinity)) return;
+        private Camera mainCamera;
+        private NavMeshHit hit;
+        #region Server
+        [Command]
+        private void CmdMove(Vector3 position)
+        {
+            if (!ValidatePosition(position)) { return; }
+            navMeshAgent.SetDestination(hit.position);
+        }
 
-        CmdMove(raycastHit.point);
+        private bool ValidatePosition(Vector3 position)
+        {
+            if (!NavMesh.SamplePosition(position, out NavMeshHit navMeshHit, 1f, NavMesh.AllAreas)) return false;
+            hit = navMeshHit;
+            return true;
+        }
+        #endregion
+        #region Client
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+        public override void OnStartAuthority()
+        {
+            base.OnStartAuthority();
+            mainCamera = Camera.main;
+        }
+        [ClientCallback]
+        private void Update()
+        {
+            //Checking if right click is valid for moving
+            if (!hasAuthority) return;
+            if (!Mouse.current.rightButton.wasPressedThisFrame) return;
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (!Physics.Raycast(ray, out RaycastHit raycastHit, Mathf.Infinity)) return;
+
+            CmdMove(raycastHit.point);
+        }
+        #endregion
     }
-    #endregion
 }
