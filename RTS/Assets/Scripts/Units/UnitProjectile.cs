b8e05eb0462bc84dd32aaef15e278305081fdf62
Ikuti <m.j.olkiewicz@gmail.com>
2021-04-03 16:21:56 +0200

    Added health script for buildings and units + fixesd firing

--- RTS/Assets/Scripts/Units/UnitProjectile.cs
+++ RTS/Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -18,7 +19,19 @@ public class UnitProjectile : NetworkBehaviour
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
     }
-
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) return;
+            if(other.TryGetComponent(out Health health))
+            {
+                health.DealDamage(damageToDeal);
+            }
+            DestroySelf();
+        }
+    }
     [Server]
     private void DestroySelf()
     {
